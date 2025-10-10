6063ce670a6fac46c5edaecf4152694bbbb27ac8
KHALID ELSEADAWY <kseadawy@gmail.com>
2025-10-09 21:38:21 -0500

    Uploaded image done as well as implemented Serilog logging

---
+++ NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace NZWalks.API.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            this._logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                // Call the next middleware in the pipeline
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var errorId = Guid.NewGuid();
+                var error = new {
+                    Id = errorId,
+                    Message = "Something went wrong!"
+                    };
+                // Log the exception
+                _logger.LogError(ex, $"{errorId}: {ex.Message}");
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; //500
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(error);
+        }
+        }
+    }
+}
---
+++ NZWalks.API/Repositories/IImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using NuGet.Protocol.Core.Types;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IImageRepository: IRepository<Image, Guid>
+    {
+        Task<Image> UploadAsync(Image image);
+    }
+}
